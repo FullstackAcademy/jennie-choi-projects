ccdd9107de9442449b121d6f4eebb9e2ab4f0125
airbornejennie <146116778+airbornejennie@users.noreply.github.com>
2023-10-23 13:59:34 -0500

    updated javascript.js file

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// Dialog box for the home page
+alert("It's a beautiful day. Don't let it get away!");
+// Simple function
+let a = "Hello Fullstack Team! ";
+let b = "Welcome to Jennie's Personal Website.";
+console.log(a + b);
